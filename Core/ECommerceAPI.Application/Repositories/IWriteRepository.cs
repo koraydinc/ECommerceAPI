ca5617460b709524d1592857a4444fb58e23b8b0
Koray Dinc <koray.dnc@hotmail.com>
2024-08-28 00:17:41 +0300

    Adding Repositories

---
+++ Core/ECommerceAPI.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : BaseEntitiy
+    {
+        Task<bool> AddAsync(T data);
+        Task<bool> AddRangeAsync(List<T> datas);
+        bool Remove(T data);
+        bool RemoveRange(List<T> datas);
+        Task<bool> RemoveAsync(string id);
+        bool Update(T data);
+        Task<int> SaveAsync();
+    }
+}
---
+++ Presentation/ECommerceAPI.API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using ECommerceAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProductReadRepository _productReadRepository;
+
+        public ProductController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+
+        [HttpGet]
+        public async void Added()
+        {
+            await _productWriteRepository.AddRangeAsync(new()
+            {
+                new Domain.Entities.Product() {Id = Guid.NewGuid(), Name = "Product-1", Price = 100, CreateDate = DateTime.UtcNow, Stock = 10},
+                new Domain.Entities.Product() {Id = Guid.NewGuid(), Name = "Product-2", Price = 200, CreateDate = DateTime.UtcNow, Stock = 20}
+            });
+            await _productWriteRepository.SaveAsync();
+        }
+    }
+}
