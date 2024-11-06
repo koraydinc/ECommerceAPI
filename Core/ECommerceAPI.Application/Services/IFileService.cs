00193456ae36691ca5bc135a35cb76f25f8f75af
Koray Dinc <koray.dnc@hotmail.com>
2024-11-06 20:53:51 +0300

    convert public FileRenameAsync method to private

--- Core/ECommerceAPI.Application/Services/IFileService.cs
+++ Core/ECommerceAPI.Application/Services/IFileService.cs
@@ -5,7 +5,6 @@ namespace ECommerceAPI.Application.Services
     public interface IFileService
     {
         Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection formFiles);
-        Task<string> FileRenameAsync(string fileName);
         Task<bool> CopyFileAsync(string path, IFormFile file);
     }
 }
